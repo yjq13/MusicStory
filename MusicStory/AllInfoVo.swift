1feecf055ef2b65ab7bc6c7918d8f35ec248310a
yjq13 <281882327@qq.com>
2015-08-09 06:54:58 +0800

    vopo

---
+++ MusicStory/AllInfoVo.swift
@@ -0,0 +1,9 @@
+//
+//  AllInfoVo.swift
+//  MusicStory
+//
+//  Created by yjq131250077 on 15/8/9.
+//  Copyright (c) 2015年 yjq131250077. All rights reserved.
+//金币等级记录
+
+import Foundation
---
+++ MusicStory/GameInfoVo.swift
@@ -0,0 +1,9 @@
+//
+//  GameInfoVo.swift
+//  MusicStory
+//
+//  Created by yjq131250077 on 15/8/9.
+//  Copyright (c) 2015年 yjq131250077. All rights reserved.
+//游戏信息
+
+import Foundation
