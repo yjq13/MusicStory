1e2cbec31ea2dea892725159f45c20d1f3e59603
yjq13 <281882327@qq.com>
2015-08-09 07:38:38 +0800

    noBug版

--- MusicStory/bl_Music_GetData.swift
+++ MusicStory/bl_Music_GetData.swift
@@ -3,11 +3,15 @@
 //  MusicStory
 //
 //  Created by yjq131250077 on 15/8/7.
-//  Copyright (c) 2015年 yjq131250077. All rights reserved.
+//  Copyright (c) 2015 yjq131250077. All rights reserved.
 //
 
 import Foundation
 
 class bl_Music_GetData:MusicGetData_BlService {
-    
+    func getMusicData(ID: String) -> MusicDataPo {
+        var musicdata:MusicDataPo
+        musicdata = MusicDataPo()
+        return musicdata
+    }
 }
