dbc74f37238777a3a29d72f42eebccd066aba3e5
mcsnb <1010885032@qq.com>
2015-08-08 18:58:00 +0800

    8.8

--- MusicStory/GameViewController.swift
+++ MusicStory/GameViewController.swift
@@ -17,13 +17,24 @@ class GameViewController: UIViewController {
         // Do any additional setup after loading the view.
         stopView.hidden=true
         //测试
-        var list1=[1.0,2.5,3.1,4.5,4.8,6.2]
-        var list2=[1.2,1.4,2.1,2.8,8.14,9.1]
-        var list3=[1.9,3.8,5.4,7.1,7.9,8.1,8,3]
-        var list4=[2.0,2.5,3.1,5.2,7.1]
-        var list5=[0.1,0.6,1.2,1.9,5.1,5.9]
-        var list6=[0.1,0.6,3.1,3.6,6.8]
-        gameStart(list1, list2: list2, list3: list3,list4: list4, list5: list5, list6: list6)
+        //游戏开始倒计时
+        var clock=UIImageView(frame: CGRectMake(100, 50, 300, 200))
+        
+        UIView.animateWithDuration(1, delay:5,
+            options:UIViewAnimationOptions.TransitionNone, animations:
+            {
+                
+                self.view.addSubview(clock)
+            },
+            completion:{
+                (finished:Bool) -> Void in
+                UIView.animateWithDuration(1.5, animations:{
+                    ()-> Void in
+                   self.gameStart()
+                })
+        })
+
+        
        
         
     }
@@ -31,12 +42,16 @@ class GameViewController: UIViewController {
     //六个按键的节奏=6个数组
     var width:CGFloat=50
     var heigth:CGFloat=20
-    func gameStart(var list1:[Double],var list2:[Double],var list3:[Double],var list4:[Double],var list5:[Double],var list6:[Double]){
-
+    func gameStart() {
+        var list1=[1.0,2.5,3.1,4.5,4.8,6.2]
+        var list2=[1.2,1.4,2.1,2.8,8.14,9.1]
+        var list3=[1.9,3.8,5.4,7.1,7.9,8.1,8,3]
+        var list4=[2.0,2.5,3.1,5.2,7.1]
+        var list5=[0.1,0.6,1.2,1.9,5.1,5.9]
+        var list6=[0.1,0.6,3.1,3.6,6.8]
         //设置动画效果
-        print(list1.count)
         for t in list1{
-            var background1 = UIView(frame:CGRectMake(40, 50, width, heigth))
+            var background1 = UIView(frame:CGRectMake(100, 50, width, heigth))
             background1.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background1)
             background1.alpha=0.0
@@ -51,14 +66,16 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background1.layer.removeFromSuperlayer()
+                       // background1.layer.removeFromSuperlayer()
+                        background1.hidden=true
                     })
             })
+            
         }
         //设置动画效果
         
         for t in list2{
-            var background2 = UIView(frame:CGRectMake(120, 50, width, heigth))
+            var background2 = UIView(frame:CGRectMake(180, 50, width, heigth))
             background2.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background2)
             background2.alpha=0.0
@@ -73,14 +90,15 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background2.layer.removeFromSuperlayer()
+                        //background2.layer.removeFromSuperlayer()
+                        background2.hidden=true
                     })
             })
         }
         
         
         for t in list3{
-            var background3 = UIView(frame:CGRectMake(200, 50, width, heigth))
+            var background3 = UIView(frame:CGRectMake(260, 50, width, heigth))
             background3.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background3)
             background3.alpha=0.0
@@ -95,7 +113,8 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background3.layer.removeFromSuperlayer()
+                        //background3.layer.removeFromSuperlayer()
+                        background3.hidden=true
                     })
             })
         }
@@ -103,7 +122,7 @@ class GameViewController: UIViewController {
         
         
         for t in list4{
-            var background4 = UIView(frame:CGRectMake(280, 50, width, heigth))
+            var background4 = UIView(frame:CGRectMake(340, 50, width, heigth))
             background4.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background4)
             background4.alpha=0.0
@@ -118,12 +137,13 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background4.layer.removeFromSuperlayer()
+                        //background4.layer.removeFromSuperlayer()
+                        background4.hidden=true
                     })
             })
         }
         for t in list5{
-            var background5 = UIView(frame:CGRectMake(360, 50, width, heigth))
+            var background5 = UIView(frame:CGRectMake(420, 50, width, heigth))
             background5.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background5)
             background5.alpha=0.0
@@ -138,12 +158,13 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background5.layer.removeFromSuperlayer()
+                       // background5.layer.removeFromSuperlayer()
+                        background5.hidden=true
                     })
             })
         }
         for t in list6{
-            var background6 = UIView(frame:CGRectMake(440, 50, width, heigth))
+            var background6 = UIView(frame:CGRectMake(500, 50, width, heigth))
             background6.backgroundColor = UIColor.darkGrayColor()
             self.view.addSubview(background6)
             background6.alpha=0.0
@@ -158,14 +179,15 @@ class GameViewController: UIViewController {
                     (finished:Bool) -> Void in
                     UIView.animateWithDuration(1.5, animations:{
                         ()-> Void in
-                        background6.layer.removeFromSuperlayer()
+                       // background6.layer.removeFromSuperlayer()
+                        background6.hidden=true
                     })
             })
         }
-        
     }
     //下面是6键模式的按键
     @IBAction func stopButton(sender: AnyObject) {
+        
         stopView.hidden=false
     }
     
@@ -187,6 +209,8 @@ class GameViewController: UIViewController {
         stopView.hidden=true
     }
     @IBAction func retryButton(sender: AnyObject) {
+        stopView.hidden=true
+        gameStart()
     }
     @IBAction func backButton(sender: AnyObject) {
     }
